bedd8317291361a370ea17e6fced4254e2bf77e9
levanmamuladze <levanmamoulatze@gmail.com>
2023-03-27 11:41:46 +0000

    Conditional Profile Card Generator done

--- src/js/app.js
+++ src/js/app.js
@@ -31,16 +31,40 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  ${cover ? cover : ""}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Lucy"} ${
+    variables.lastname ? variables.lastname : "Boillet"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Web Developer"}</h2>
+          <h3>${variables.city ? variables.city : "Miami"} ${
+    variables.country ? variables.country : "USA"
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition
+              ? variables.socialMediaPosition
+              : "position-right"
+          }">
+            <li><a href=${
+              variables.twitter
+                ? variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+                ? variables.github
+                : "https://github.com/4geeksacademy"
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://linkedin.com/4geeksacademy"
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+                ? variables.instagram
+                : "https://instagram.com/4geeksacademy"
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
